d270ef35831207f77233ab87c154c5f601a15552
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-13 19:26:07 +0200

    Updates

---
+++ precache-manifest.7883d499f015b7450f400b783c409b60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56072ff399de0671783849cdaec93c5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bf600228ea786f849d6",
+    "url": "/static/css/main.1d7c60b6.chunk.css"
+  },
+  {
+    "revision": "0fda26cc049aca74654b",
+    "url": "/static/js/2.e31f694c.chunk.js"
+  },
+  {
+    "revision": "9bf600228ea786f849d6",
+    "url": "/static/js/main.6f599219.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
