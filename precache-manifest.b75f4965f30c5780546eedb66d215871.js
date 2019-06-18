740bef3c40d61f1d9576342c5bf8339faaa473cf
Michał Janiczko <michaljaniczko1@gmail.com>
2019-06-18 21:58:42 +0200

    Updates

--- precache-manifest.b75f4965f30c5780546eedb66d215871.js
+++ precache-manifest.b75f4965f30c5780546eedb66d215871.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5cb8d217aa7068c0cc1f498fb7647124",
+    "revision": "7523c0563580a12af02faeb5437d3c92",
     "url": "/index.html"
   },
   {
-    "revision": "79fa4e1ba1ab733dd404",
-    "url": "/static/css/main.499dce1a.chunk.css"
+    "revision": "9bf600228ea786f849d6",
+    "url": "/static/css/main.1d7c60b6.chunk.css"
   },
   {
     "revision": "0fda26cc049aca74654b",
     "url": "/static/js/2.e31f694c.chunk.js"
   },
   {
-    "revision": "79fa4e1ba1ab733dd404",
-    "url": "/static/js/main.bdb62360.chunk.js"
+    "revision": "9bf600228ea786f849d6",
+    "url": "/static/js/main.6f599219.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
